a7b74598ba210077527c7b70a3aefdf8710a64f2
Walter Gunter <wegunterjr@gmail.com>
2016-05-30 07:03:17 +0000

    adding new change to see if jenkins can detect it...travis is all happy...

--- src/hello.cpp
+++ src/hello.cpp
@@ -4,5 +4,6 @@ int main(int argc, char *argv[])
 {
     std::cout  << "Hello World!" << std::endl;
     std::cout <<  "2nd Line!" << std::endl;
+    std::cout << "3rd line! maybe it will see the change?!" << std::endl;
     return 0;
 }
